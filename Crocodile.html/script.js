e2f99a556351023a5fef984a05e6b443665f6308
Rahul patidar <141323154+patidarrahul0509@users.noreply.github.com>
2024-07-01 13:08:21 +0530

    Add files via upload

---
+++ Crocodile.html/script.js
@@ -0,0 +1,197 @@
+document.querySelectorAll(".header-lf a")[1].addEventListener("mouseenter",function(){
+    document.querySelectorAll(".data")[0].classList.add("show")
+
+    document.querySelectorAll(".data")[0].addEventListener("mouseleave",function(){
+    document.querySelectorAll(".data")[0].classList.remove("show")
+
+    })
+
+})
+
+document.querySelectorAll(".header-lf a")[2].addEventListener("mouseenter",function(){
+    document.querySelectorAll(".data")[0].classList.add("show")
+
+    document.querySelectorAll(".data")[0].addEventListener("mouseleave",function(){
+    document.querySelectorAll(".data")[0].classList.remove("show")
+
+    })
+
+})
+
+document.querySelectorAll(".header-lf a")[3].addEventListener("mouseenter",function(){
+    document.querySelectorAll(".data")[0].classList.add("show")
+
+    document.querySelectorAll(".data")[0].addEventListener("mouseleave",function(){
+    document.querySelectorAll(".data")[0].classList.remove("show")
+
+    })
+
+})
+
+document.querySelectorAll(".header-lf a")[4].addEventListener("mouseenter",function(){
+    document.querySelectorAll(".data")[0].classList.add("show")
+
+    document.querySelectorAll(".data")[0].addEventListener("mouseleave",function(){
+    document.querySelectorAll(".data")[0].classList.remove("show")
+
+    })
+
+})
+
+document.querySelectorAll(".school")[0].addEventListener("click",function(){
+    document.querySelector(".in-rt").innerHTML = `
+    
+    <div class="box">
+    <div class="bx">
+        <img src="https://wallpapers.com/images/featured/crocodile-pictures-3wwg20uxh0h2smxp.jpg" alt="">
+        <h2>heading</h2>
+    </div>
+    <div class="bx">
+        <img src="https://wallpapers.com/images/featured/crocodile-pictures-3wwg20uxh0h2smxp.jpg" alt="">
+        <h2>heading</h2>
+    </div>
+    <div class="bx">
+        <img src="https://wallpapers.com/images/featured/crocodile-pictures-3wwg20uxh0h2smxp.jpg" alt="">
+        <h2>heading</h2>
+    </div>
+    <div class="bx">
+        <img src="https://wallpapers.com/images/featured/crocodile-pictures-3wwg20uxh0h2smxp.jpg" alt="">
+        <h2>heading</h2>
+    </div>
+    <div class="bx">
+        <img src="https://wallpapers.com/images/featured/crocodile-pictures-3wwg20uxh0h2smxp.jpg" alt="">
+        <h2>heading</h2>
+    </div>
+    <div class="bx">
+    <img src="https://wallpapers.com/images/featured/crocodile-pictures-3wwg20uxh0h2smxp.jpg" alt="">
+    <h2>heading</h2>
+    </div>
+    <div class="bx">
+    <img src="https://wallpapers.com/images/featured/crocodile-pictures-3wwg20uxh0h2smxp.jpg" alt="">
+    <h2>heading</h2>
+    </div>
+    <div class="bx">
+    <img src="https://wallpapers.com/images/featured/crocodile-pictures-3wwg20uxh0h2smxp.jpg" alt="">
+    <h2>heading</h2>
+    </div>
+</div>
+
+    
+    `
+    document.querySelector(".box").addEventListener("mouseleave",function(){
+        document.querySelector(".in-rt").innerHTML = ` <img src="https://wallpapercave.com/wp/wp4080198.jpg" alt="">`
+    })
+
+})
+document.querySelectorAll(".school")[1].addEventListener("click",function(){
+    document.querySelector(".in-rt").innerHTML = `
+    
+    <div class="box">
+    <div class="bx">
+        <img src="https://wallpapers.com/images/featured/crocodile-pictures-3wwg20uxh0h2smxp.jpg" alt="">
+        <h2>heading</h2>
+    </div>
+    <div class="bx">
+        <img src="https://wallpapers.com/images/featured/crocodile-pictures-3wwg20uxh0h2smxp.jpg" alt="">
+        <h2>heading</h2>
+    </div>
+    <div class="bx">
+        <img src="https://wallpapers.com/images/featured/crocodile-pictures-3wwg20uxh0h2smxp.jpg" alt="">
+        <h2>heading</h2>
+    </div>
+    <div class="bx">
+        <img src="https://wallpapers.com/images/featured/crocodile-pictures-3wwg20uxh0h2smxp.jpg" alt="">
+        <h2>heading</h2>
+    </div>
+    <div class="bx">
+        <img src="https://wallpapers.com/images/featured/crocodile-pictures-3wwg20uxh0h2smxp.jpg" alt="">
+        <h2>heading</h2>
+    </div>
+    <div class="bx">
+    <img src="https://wallpapers.com/images/featured/crocodile-pictures-3wwg20uxh0h2smxp.jpg" alt="">
+    <h2>heading</h2>
+</div>
+</div>
+
+    
+    `
+    document.querySelector(".box").addEventListener("mouseleave",function(){
+        document.querySelector(".in-rt").innerHTML = ` <img src="https://wallpapercave.com/wp/wp4080198.jpg" alt="">`
+    })
+
+})
+document.querySelectorAll(".school")[2].addEventListener("click",function(){
+    document.querySelector(".in-rt").innerHTML = `
+    
+    <div class="box">
+    <div class="bx">
+        <img src="https://wallpapers.com/images/featured/crocodile-pictures-3wwg20uxh0h2smxp.jpg" alt="">
+        <h2>heading</h2>
+    </div>
+    <div class="bx">
+        <img src="https://wallpapers.com/images/featured/crocodile-pictures-3wwg20uxh0h2smxp.jpg" alt="">
+        <h2>heading</h2>
+    </div>
+    <div class="bx">
+        <img src="https://wallpapers.com/images/featured/crocodile-pictures-3wwg20uxh0h2smxp.jpg" alt="">
+        <h2>heading</h2>
+    </div>
+    <div class="bx">
+        <img src="https://wallpapers.com/images/featured/crocodile-pictures-3wwg20uxh0h2smxp.jpg" alt="">
+        <h2>heading</h2>
+    </div>
+    <div class="bx">
+        <img src="https://wallpapers.com/images/featured/crocodile-pictures-3wwg20uxh0h2smxp.jpg" alt="">
+        <h2>heading</h2>
+    </div>
+    <div class="bx">
+    <img src="https://wallpapers.com/images/featured/crocodile-pictures-3wwg20uxh0h2smxp.jpg" alt="">
+    <h2>heading</h2>
+</div>
+</div>
+
+    
+    `
+    document.querySelector(".box").addEventListener("mouseleave",function(){
+        document.querySelector(".in-rt").innerHTML = ` <img src="https://wallpapercave.com/wp/wp4080198.jpg" alt="">`
+    })
+
+})
+document.querySelectorAll(".school")[3].addEventListener("click",function(){
+    document.querySelector(".in-rt").innerHTML = `
+    
+    <div class="box">
+    <div class="bx">
+        <img src="https://wallpapers.com/images/featured/crocodile-pictures-3wwg20uxh0h2smxp.jpg" alt="">
+        <h2>heading</h2>
+    </div>
+    <div class="bx">
+        <img src="https://wallpapers.com/images/featured/crocodile-pictures-3wwg20uxh0h2smxp.jpg" alt="">
+        <h2>heading</h2>
+    </div>
+    <div class="bx">
+        <img src="https://wallpapers.com/images/featured/crocodile-pictures-3wwg20uxh0h2smxp.jpg" alt="">
+        <h2>heading</h2>
+    </div>
+    <div class="bx">
+        <img src="https://wallpapers.com/images/featured/crocodile-pictures-3wwg20uxh0h2smxp.jpg" alt="">
+        <h2>heading</h2>
+    </div>
+    <div class="bx">
+        <img src="https://wallpapers.com/images/featured/crocodile-pictures-3wwg20uxh0h2smxp.jpg" alt="">
+        <h2>heading</h2>
+    </div>
+    <div class="bx">
+    <img src="https://wallpapers.com/images/featured/crocodile-pictures-3wwg20uxh0h2smxp.jpg" alt="">
+    <h2>heading</h2>
+</div>
+</div>
+
+    
+    `
+    document.querySelector(".box").addEventListener("mouseleave",function(){
+        document.querySelector(".in-rt").innerHTML = ` <img src="https://wallpapercave.com/wp/wp4080198.jpg" alt="">`
+    })
+
+})
+
